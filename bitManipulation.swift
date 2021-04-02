91a7979f5b158519dbdd3c71ec58cc781a360c56
jericho <baofeng.qian@maitao.com>
2021-04-02 09:38:19 +0800

    feat(bitManipution):

---
+++ bitManipulation.swift
@@ -0,0 +1,45 @@
+// Missing Number 268
+
+// Given an array nums containing n distinct number in the range [0,n], return the only number in the range that is missing from the array.
+
+// follow up: could you implement a sloution using only O(1) extra space complexity and O(n) runtime complexity
+
+class Solution {
+    func missingNumber(_ numberarray:[Int]) -> Int {
+        let length = numberarray.count
+        var mutableArray:[Int] = Array(0...length);
+            for number in numberarray {
+                for (index,i) in mutableArray.enumerated() {
+                    if number == i {
+                        mutableArray.remove(at: index)
+                    }
+                }
+            }
+        return mutableArray[0];
+    }
+    // 上述写法已经超出时间限制了，所以应该需要找别的办法，再除了为运算之外还有别的好办法吗？有没有办法只便利一次呢？
+    // 先加，求总合 再减，这样时间复杂度就只有 O(n) 了
+    func missingNumber2(_ numberarray:[Int]) -> Int {
+        let length = numberarray.count
+        var totalSum = (length * (length + 1)) / 2
+        
+            for number in numberarray {
+                totalSum -= number
+            }
+        return totalSum;
+    }
+    
+    // 当然，异或运算
+    
+    func missingNumber3(_ nums:[Int]) -> Int {
+        var miss = nums.count
+        for i in 0..<miss {
+            miss ^= i ^ nums[i]
+        }
+
+        return miss;
+    }
+    
+
+}
+
